24a0b6b5a0e973502807f80484b95e0a2ef5260e
abhajaiswal-sketch <abha.jaiswal@geeksofgurukul.com>
2025-09-28 22:43:25 +0530

    Update cryptocourt.sol

--- contracts/cryptocourt.sol
+++ contracts/cryptocourt.sol
@@ -15,7 +15,13 @@ contract CryptoCourt {
         uint256 resolvedAt;
         address winner;
     }
-    
+    enum DisputeStatus {
+        Created,
+        ArbitratorAssigned,
+        InProgress,
+        Resolved,
+        Cancelled
+    }
    
     
     // State variables
